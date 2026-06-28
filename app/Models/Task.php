eafc57a225644eaceb483afe1b27ea4161d274a1
Daniel Velásquez <danielvel1977@gmail.com>
2026-06-28 15:02:07 -0600

    feat: add migrations and Eloquent models with relationships

---
+++ app/Models/Task.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'date_limit',
+        'project_id',
+        'user_id'
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+}
